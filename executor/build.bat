96724c3cf6cad964cdb5cbd9fc9ab15d8ed9f31d
Yui100901 <973800960@qq.com>
2024-08-17 13:36:11 +0800

    修改执行器的编译脚本

--- executor/build.bat
+++ executor/build.bat
@@ -1,5 +1,8 @@
 @echo off
 set EXECUTABLE_NAME=executor
+set SOURCE_NAME=executor
+set DARWIN_TAG=_darwin
+set LINUX_TAG=_linux
 REM 设置项目目录和可执行文件名
 set PROJECT_DIR=C:\Users\yfy2001\yfy\Learn\Projects\dominant\executor
 go env -w GOPROXY=https://goproxy.cn
@@ -15,11 +18,11 @@ REM 编译 macOS 版本
 echo Compiling for macOS...
 set GOOS=darwin
 set GOARCH=amd64
-go build -o %EXECUTABLE_NAME%-mac %EXECUTABLE_NAME%_darwin.go
+go build -o %EXECUTABLE_NAME%-mac %EXECUTABLE_NAME%%DARWIN_TAG%.go
 REM 编译 Linux 版本
 echo Compiling for Linux...
 set GOOS=linux
 set GOARCH=amd64
-go build -o %EXECUTABLE_NAME%-linux %EXECUTABLE_NAME%_linux.go
+go build -o %EXECUTABLE_NAME%-linux %EXECUTABLE_NAME%%LINUX_TAG%.go
 echo Compilation finished.
 pause
