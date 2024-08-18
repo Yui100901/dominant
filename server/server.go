22ebcbd2d772772593867be69923f3a3d43a39c8
Yui100901 <973800960@qq.com>
2024-08-18 10:13:55 +0800

    解决跨域

--- server/server.go
+++ server/server.go
@@ -1,6 +1,9 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 const (
 	IP      = "127.0.0.1" //控制端连接IP
@@ -8,7 +11,24 @@ const (
 	BaseUrl = "http://" + IP + ":" + Port
 )
 
+func Cors() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		method := context.Request.Method
+
+		context.Header("Access-Control-Allow-Origin", "*")
+		context.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token, x-token")
+		context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH, PUT")
+		context.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		context.Header("Access-Control-Allow-Credentials", "true")
+
+		if method == "OPTIONS" {
+			context.AbortWithStatus(http.StatusNoContent)
+		}
+	}
+}
+
 func NewServer() *gin.Engine {
 	r := gin.Default()
+	r.Use(Cors())
 	return r
 }
