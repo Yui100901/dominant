213a0e5bc5a04891205a3101270d5c6781ff5eeb
Yui100901 <973800960@qq.com>
2024-08-21 21:15:23 +0800

    publisher封装

--- mqttutil/publisher/publisher.go
+++ mqttutil/publisher/publisher.go
@@ -9,10 +9,30 @@ import (
 // @Date 2024/8/15 15 58
 //
 
-func PublishTelemetry(client mqtt.Client, topic string, payload any) {
-	if conn := client.Connect(); conn.Wait() && conn.Error() != nil {
-		panic(conn.Error())
+type Publisher struct {
+	clientId string
+	Client   mqtt.Client
+}
+
+func NewPublisher(id string) *Publisher {
+	opts := mqtt.NewClientOptions()
+	opts.SetClientID(id)
+	opts.SetDefaultPublishHandler(DefaultPublishHandler)
+	client := mqtt.NewClient(nil)
+	return &Publisher{
+		clientId: id,
+		Client:   client,
 	}
-	res := client.Publish(topic, 0, false, payload)
+}
+
+func (p *Publisher) Publish(topic string, payload any) {
+	//if conn := client.Connect(); conn.Wait() && conn.Error() != nil {
+	//	panic(conn.Error())
+	//}
+	res := p.Client.Publish(topic, 0, false, payload)
 	res.Wait()
+}
+
+func DefaultPublishHandler(client mqtt.Client, msg mqtt.Message) {
+
 }
