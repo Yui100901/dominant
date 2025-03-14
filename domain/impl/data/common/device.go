4d455442f7435841a69d872a3162c1de007d30a5
Yui100901 <973800960@qq.com>
2025-03-14 12:49:03 +0800

    修改设备接口定义

--- domain/impl/data/common/device.go
+++ domain/impl/data/common/device.go
@@ -1,8 +1,6 @@
 package common
 
 import (
-	"dominant/infrastructure/message"
-	"github.com/google/uuid"
 	"time"
 )
 
@@ -13,16 +11,22 @@ import (
 
 var deviceVideoLinkMap map[string]string
 
+type DeviceMessage interface {
+	ConvertToDevice(id string) *Device
+	ConvertToTelemetry(deviceId string) *Telemetry
+}
+
 type Device struct {
-	ID            string     `json:"id"`            //id
-	Name          string     `json:"name"`          //名称
-	DeviceType    string     `json:"deviceType"`    //设备类型
-	EnvType       string     `json:"envType"`       //环境类型
-	Model         string     `json:"model"`         //设备型号
-	LastTelemetry *Telemetry `json:"lastTelemetry"` //最新遥测数据
+	ID         string `json:"id"`         //id
+	Name       string `json:"name"`       //名称
+	DeviceType string `json:"deviceType"` //设备类型
+	EnvType    string `json:"envType"`    //环境类型
+	Model      string `json:"model"`      //设备型号
 }
 
 type Telemetry struct {
+	ID            string    `json:"id"`            //id
+	DeviceID      string    `json:"deviceId"`      //设备id
 	TelemetryTime time.Time `json:"telemetryTime"` //遥测上传时间
 	Position      *Position `json:"position"`      //位置
 	Status        *Status   `json:"status"`        //状态
@@ -41,34 +45,7 @@ type Status struct {
 	BatteryLife int     `json:"batteryLife"` //续航时间
 }
 
-func (d *Device) ConvertToMessage() *message.Message {
-	return message.NewMessage(d.ID, d)
-}
-
 func init() {
 	deviceVideoLinkMap = make(map[string]string)
 	deviceVideoLinkMap["9dd4299f15f74ffb8d52a39ffc1d2dc7"] = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
-}
-
-func (d *Device) ConvertToDeviceData() *DeviceData {
-	dataId := uuid.NewString()
-	return &DeviceData{
-		Id:            dataId,
-		DeviceId:      d.ID,
-		DeviceType:    d.DeviceType,
-		Name:          d.Name,
-		Model:         d.Model,
-		IsOnline:      1,
-		TelemetryTime: d.LastTelemetry.TelemetryTime.Format("2006-01-02 15:04:05"),
-		Longitude:     d.LastTelemetry.Position.Longitude,
-		Latitude:      d.LastTelemetry.Position.Latitude,
-		Height:        d.LastTelemetry.Position.Height,
-		Velocity:      d.LastTelemetry.Status.Velocity,
-		Battery:       d.LastTelemetry.Status.Battery,
-		BatteryLife:   d.LastTelemetry.Status.BatteryLife,
-		EnvType:       d.EnvType,
-		VideoLink:     deviceVideoLinkMap[d.ID],
-		IsVideoStream: false,
-		RawData:       d.LastTelemetry.RawData,
-	}
 }
--- domain/impl/data/common/device_data.go
+++ domain/impl/data/common/device_data.go
@@ -1,11 +1,15 @@
 package common
 
+import (
+	"github.com/google/uuid"
+)
+
 //
 // @Author yfy2001
 // @Date 2025/1/21 14 10
 //
 
-type DeviceData struct {
+type DeviceTelemetryData struct {
 	Id            string  `json:"id"`
 	DeviceId      string  `json:"deviceId"`
 	DeviceType    string  `json:"deviceType"`
@@ -23,4 +27,27 @@ type DeviceData struct {
 	VideoLink     string  `json:"videoLink"`
 	IsVideoStream bool    `json:"isVideoStream"`
 	RawData       any     `json:"rawData"`
+}
+
+func NewDeviceTelemetryData(device *Device, telemetry *Telemetry) *DeviceTelemetryData {
+	dataId := uuid.NewString()
+	return &DeviceTelemetryData{
+		Id:            dataId,
+		DeviceId:      device.ID,
+		DeviceType:    device.DeviceType,
+		EnvType:       device.EnvType,
+		Name:          device.Name,
+		Model:         device.Model,
+		IsOnline:      1,
+		TelemetryTime: telemetry.TelemetryTime.Format("2006-01-02 15:04:05"),
+		Longitude:     telemetry.Position.Longitude,
+		Latitude:      telemetry.Position.Latitude,
+		Height:        telemetry.Position.Height,
+		Velocity:      telemetry.Status.Velocity,
+		Battery:       telemetry.Status.Battery,
+		BatteryLife:   telemetry.Status.BatteryLife,
+		VideoLink:     deviceVideoLinkMap[device.ID],
+		IsVideoStream: false,
+		RawData:       telemetry.RawData,
+	}
 }
