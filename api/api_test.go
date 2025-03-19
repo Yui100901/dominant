250d4617571036f36b4511960c30322fcb95eab1
Yui100901 <973800960@qq.com>
2025-03-19 14:21:48 +0800

    修改api测试

--- api/api_test.go
+++ api/api_test.go
@@ -10,13 +10,23 @@ import (
 // @Date 2025/1/21 13 48
 //
 
+const (
+	IP      = "127.0.0.1"
+	PORT    = "28888"
+	ADDR    = IP + ":" + PORT
+	BaseUrl = "http://" + IP + ":" + PORT
+)
+
 func TestDeviceMessage(t *testing.T) {
+	apiUrl := BaseUrl + UrlPrefix + "/telemetry/dataInfo"
 	hc := http_utils.NewHTTPClient()
 	res, _ := hc.SendRequest(
 		http_utils.NewHTTPRequest(
-			"GetByQuery",
-			"http://127.0.0.1:27777/api/v3/telemetry/dataInfo",
+			"GET",
+			apiUrl,
 			nil,
+			nil,
+			"",
 			nil))
 	t.Log(string(res))
 }
