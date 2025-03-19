8dfffcab16b6f15a89565accd93123b7425e307e
Yui100901 <973800960@qq.com>
2025-03-19 14:23:20 +0800

    增加多个全局表，用于暂时存储数据

--- global/global.go
+++ global/global.go
@@ -11,7 +11,15 @@ import (
 //
 
 var DeviceMap *concurrent.SafeMap[string, *common.Device]
+var TelemetryLatestMap *concurrent.SafeMap[string, *common.Telemetry]
+var TelemetryMap *concurrent.SafeMap[string, *common.Telemetry]
+var DeviceTelemetryDataLatestMap *concurrent.SafeMap[string, *common.DeviceTelemetryData]
+var VirtualDeviceTelemetryDataMap *concurrent.SafeMap[string, *common.DeviceTelemetryData]
 
 func init() {
 	DeviceMap = concurrent.NewSafeMap[string, *common.Device](32)
+	TelemetryLatestMap = concurrent.NewSafeMap[string, *common.Telemetry](32)
+	TelemetryMap = concurrent.NewSafeMap[string, *common.Telemetry](32)
+	DeviceTelemetryDataLatestMap = concurrent.NewSafeMap[string, *common.DeviceTelemetryData](32)
+	VirtualDeviceTelemetryDataMap = concurrent.NewSafeMap[string, *common.DeviceTelemetryData](32)
 }
