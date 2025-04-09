c72487fef1ba5318530d912993d4dae7d65e2037
Yui100901 <973800960@qq.com>
2025-04-09 15:00:21 +0800

    完删除无用的全局map

--- global/global.go
+++ global/global.go
@@ -2,7 +2,7 @@ package global
 
 import (
 	"dominant/domain/impl/data/common"
-	"github.com/Yui100901/MyGo/concurrent"
+	"github.com/Yui100901/MyGo/concurrency"
 )
 
 //
@@ -10,16 +10,8 @@ import (
 // @Date 2025/1/8 20 43
 //
 
-var DeviceMap *concurrent.SafeMap[string, *common.Device]
-var TelemetryLatestMap *concurrent.SafeMap[string, *common.Telemetry]
-var TelemetryMap *concurrent.SafeMap[string, *common.Telemetry]
-var DeviceTelemetryDataLatestMap *concurrent.SafeMap[string, *common.DeviceTelemetryData]
-var VirtualDeviceTelemetryDataMap *concurrent.SafeMap[string, *common.DeviceTelemetryData]
+var DeviceMap *concurrency.SafeMap[string, *common.Device]
 
 func init() {
-	DeviceMap = concurrent.NewSafeMap[string, *common.Device](32)
-	TelemetryLatestMap = concurrent.NewSafeMap[string, *common.Telemetry](32)
-	TelemetryMap = concurrent.NewSafeMap[string, *common.Telemetry](32)
-	DeviceTelemetryDataLatestMap = concurrent.NewSafeMap[string, *common.DeviceTelemetryData](32)
-	VirtualDeviceTelemetryDataMap = concurrent.NewSafeMap[string, *common.DeviceTelemetryData](32)
+	DeviceMap = concurrency.NewSafeMap[string, *common.Device](32)
 }
